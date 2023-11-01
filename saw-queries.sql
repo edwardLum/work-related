153a65e6033dcfb6d7cae6c50d8f13304317dbbb
Dimitris Lyrintzis <dim.lyrintzis@gmail.com>
2023-11-01 15:36:39 +0200

    Added fb cost query for performance_report

--- saw-queries.sql
+++ saw-queries.sql
@@ -1,10 +1,23 @@
-/* Get daily facebook spend per account */
+/* Get daily facebook spend per account (facebook_data table) */
 SELECT
   account,
   SUM(cost)
 FROM
-  `afd-reporting.spotawheel.facebook_data`
+  `facebook_data`
 WHERE
   date="2023-10-31"
 GROUP BY
   account
+
+/* Get daily facebook spend per account from the performance_report table */
+SELECT
+  country,
+  SUM(cost)
+FROM
+  `performance_report`
+WHERE
+  date="2023-10-31" AND
+  source_medium="facebook / cpc"
+GROUP BY
+  country
+
