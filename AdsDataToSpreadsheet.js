504ee7be89be624b23596dc358eeaf0629bc2d1a
Dimitris Lyrintzis <dim.lyrintzis@gmail.com>
2023-11-23 18:56:31 +0200

    Fixed columns and added monthToDate data

--- AdsDataToSpreadsheet.js
+++ AdsDataToSpreadsheet.js
@@ -2,15 +2,18 @@ function main() {
   var dates = generateReportDates();
   
   weekToDateData = getPerformanceDataForDateRange(dates.startOfWeek, dates.end)
+  monthToDateData = getPerformanceDataForDateRange(dates.startOfMonth, dates.end)
   
   // The ID of the Google Sheets document
   var sheetId = '1KOHXY21IH7gX7CmbubUZ1_QV0fBb_EeEcQNl8gRDJQM';
 
   // The name of the sheet within the document
-  var sheetName = 'WTD Perfomance';
+  var weekToDateSheetName = 'WTD Performance';
+  var monthToDateSheetName = 'MTD Performance';
 
-  // Call the function to write data to the specific sheet
-  writeDataToSheet(weekToDateData, sheetId, sheetName);
+  // Call the function to write data to the respective sheets
+  writeDataToSheet(weekToDateData, sheetId, weekToDateSheetName);
+  writeDataToSheet(monthToDateData, sheetId, monthToDateSheetName);
 }
 
 function getPerformanceDataForDateRange(startDate, endDate) {
@@ -26,11 +29,13 @@ function getPerformanceDataForDateRange(startDate, endDate) {
   while (rows.hasNext()) {
     var row = rows.next();
     var campaignName = row['CampaignName'];
-    var impressions = row['Impressions'];
-    var clicks = row['Clicks'];
     var cost = row['Cost'];
+    var clicks = row['Clicks'];
+    var impressions = row['Impressions'];
+    var conversions = row['Conversions']; 
 
-    data.push([campaignName, impressions, clicks, cost]);
+
+    data.push([campaignName, cost, clicks, impressions, conversions]);
   }
   
   return data;
@@ -86,7 +91,7 @@ function writeDataToSheet(data, sheetId, sheetName) {
     sheet.clearContents();
 
     // Set up the header row
-    var headers = ['Campaign Name', 'Impressions', 'Clicks', 'Cost'];
+    var headers = ['Campaign Name', 'Cost', 'Clicks', 'Impressions', 'Conversions'];
     sheet.getRange(1, 1, 1, headers.length).setValues([headers]);
 
     // Write the data to the sheet starting from the second row
